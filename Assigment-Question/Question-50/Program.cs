f4e6d82d3a17485953dfa8561abc1d246cfc4c47
Faiza <faizaashraf0786@gmail.com>
2021-03-30 23:41:38 +0500

    Added Question-50

--- Assigment-Question/Question-50/Program.cs
+++ Assigment-Question/Question-50/Program.cs
@@ -6,7 +6,16 @@ namespace Question_50
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("x*x + 3*x - 4");
+            Console.WriteLine("(x+y) *z");                         
+            Console.WriteLine("(x+ 3*y)/(2*y- x)");     
+            Console.WriteLine("1/ (x*x + x + 3)");      
+            Console.WriteLine("(x+y)/7");            
+            Console.WriteLine("2*b*c*c*c");         
+            Console.WriteLine("x= (3*y)/(5-z)");        
+            Console.WriteLine("z=area*Math.Sqrt(area)");
+            Console.WriteLine("(x+32)/(y-32)-(x-2*y)"); 
+            Console.WriteLine("res = (3*i*j*k+Math.Pow(k,9))/(7*i*k-5*Math.Sqrt(j+k))");
         }
     }
 }
